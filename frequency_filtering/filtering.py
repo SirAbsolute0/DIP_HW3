ad7e8d54b98779afbb239a10d90891856c7bc199
SirAbsolute0 <53980704+SirAbsolute0@users.noreply.github.com>
2022-04-24 12:20:15 -0500

    finished part 3 and commented

--- frequency_filtering/filtering.py
+++ frequency_filtering/filtering.py
@@ -1,10 +1,9 @@
 # For this part of the assignment, You can use inbuilt functions to compute the fourier transform
 # You are welcome to use fft that are available in numpy and opencv
 
-from unittest import result
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+
 class Filtering:
 
     def __init__(self, image):
@@ -21,6 +20,7 @@ class Filtering:
         shape: the shape of the mask to be generated
         rtype: a 2d numpy array with size of shape
         """
+        #Initiate my own mask based on the given picture DFT on a frequency domain
         filter = np.ones(shape)
         filter[220:240, 285:305] = 0
         filter[265:275, 275:285] = 0
@@ -40,6 +40,8 @@ class Filtering:
         3. You can take negative (255 - fsimage)
         4. etc.
         """
+        #Thie post_process_image function is only for viewing fft image
+        #Calculate magnitude, take log and then do full contrast stretch
         magnitude = np.abs(image)
         result_img = np.log(magnitude)
         stretch_img = result_img.copy()
@@ -71,29 +73,37 @@ class Filtering:
         Note: You do not have to do zero padding as discussed in class, the inbuilt functions takes care of that
         filtered image, magnitude of frequency_filtering, magnitude of filtered frequency_filtering: Make sure all images being returned have grey scale full contrast stretch and dtype=uint8
         """
+        #Get local copy of image
         local_img = self.image.copy()
         
+        #performed fft and shift from original image
         fft_img = np.fft.ifftshift(np.fft.fft2(local_img))
 
-        result_filtered = fft_img.copy()
+        #copy out a version of the fft_image for display
         result_filtered_display = self.post_process_image(fft_img)
+
+        #Initiate the filter/mask
         filter = self.get_mask(local_img.shape)
 
+        #Apply the filter to the fft_image
+        result_filtered = fft_img.copy()
         for x in range(0, result_filtered.shape[0]):
             for y in range(0, result_filtered.shape[1]):
                 result_filtered[x, y] = fft_img[x, y] * filter[x, y]
                 result_filtered_display[x, y] = result_filtered_display[x, y] * filter[x, y]
         
+        #shift and do ifft on the image with filter applied
         final_img = np.fft.ifft2(np.fft.ifftshift(result_filtered))
 
-        test = np.abs(final_img)    
+        #calculate magnitude and do full contrast stretch to display the result image
+        result = np.abs(final_img)    
 
-        min = np.amin(test)
-        max = np.amax(test)
+        min = np.amin()
+        max = np.amax(result)
         
-        print(test.real)
-        for x in range(0, test.shape[0]):
-            for y in range(0, test.shape[1]):
-                test[x, y] = int((254/(max - min)) * (test[x, y] - min) + 0.5)
+        print(result.real)
+        for x in range(0, result.shape[0]):
+            for y in range(0, result.shape[1]):
+                result[x, y] = int((254/(max - min)) * (result[x, y] - min) + 0.5)
         
-        return [self.post_process_image(fft_img), result_filtered_display, test ]
+        return [self.post_process_image(fft_img), result_filtered_display, result ]
